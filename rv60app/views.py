952b2e6bd1e96878afc75ba4fcf4c6ef5ac5997a
keybyme <wesnetwork@keybyme.com>
2025-01-31 13:38:59 -0500

    My

--- rv60app/views.py
+++ rv60app/views.py
@@ -318,8 +318,8 @@ def timeline(request):
         ).order_by('-desde')
     else:
         timelines_queryset = Timeline.objects.all().order_by('-desde')
-    # Set up pagination (5 items per page, you can adjust this number)
-    paginator = Paginator(timelines_queryset, 5)
+    # Set up pagination (25 items per page, you can adjust this number)
+    paginator = Paginator(timelines_queryset, 25)
     page_obj = paginator.get_page(page_number)    
     print(page_obj)
     differences = []
